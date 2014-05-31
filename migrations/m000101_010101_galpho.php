668a358fb98f45eed7f631ae635a928de225ad3e
athos99 <athos99@bluewin.ch>
2014-06-01 00:58:42 +0200

    update db definition

--- migrations/m000101_010101_galpho.php
+++ migrations/m000101_010101_galpho.php
@@ -6,6 +6,87 @@ class m000101_010101_galpho extends \yii\db\Migration
 {
     public function up()
     {
+
+        $this->createTable('tbl_user', array(
+                'id' => 'pk',
+                'username' => 'VARCHAR(64) NULL',
+                'email' => 'VARCHAR(100) NULL',
+                'validated' => 'boolean DEFAULT 0',
+                'active' => 'boolean DEFAULT 0',
+                'superuser' => 'boolean DEFAULT 0',
+                'create' => 'datetime',
+                'last_login' => 'datetime',
+            ),
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        $this->insert('tbl_user', array(
+            'id' => 1,
+            'username' => 'admin',
+            'active' => 1,
+            'validated' => 1,
+            'superuser' => 1,
+            'create' => new \yii\db\Expression('NOW()'),
+        ));
+
+
+        $this->createTable('tbl_user_authenticate', array(
+                'id' => 'pk',
+                'user_id' => 'integer NOT NULL',
+                'provider' => 'VARCHAR(64) NULL',
+                'identifier' => 'VARCHAR(256) NULL',
+                'authenticate' => 'VARCHAR(512) NULL',
+                'user_data' => 'text',
+                'expire' => 'datetime NULL',
+                'active' => 'boolean DEFAULT 0',
+            ),
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        $this->createIndex('i_user_id', 'tbl_user_authenticate', 'user_id', false);
+        $this->addForeignKey('fk_user_authenticate_user_id', 'tbl_user_authenticate',
+            'user_id', 'tbl_user', 'id', 'CASCADE', 'CASCADE');
+
+
+        $this->createTable('tbl_user_field', array(
+                'id' => 'pk',
+                'user_id' => 'integer NOT NULL',
+                'field' => 'VARCHAR(64) NOT NULL',
+                'value' => 'text',
+            ),
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        $this->addForeignKey('fk_user_field_user_id', 'tbl_user_field', 'user_id',
+            'tbl_user', 'id', 'CASCADE', 'CASCADE');
+
+
+        $this->createTable('tbl_auth_item', array(
+                'name' => 'VARCHAR(64) NOT NULL PRIMARY KEY',
+                'type' => 'integer NOT NULL',
+                'description' => 'text',
+                'biz_rule' => 'text',
+                'data' => 'text',
+            ),
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        $this->createIndex('i_type', 'tbl_auth_item', 'type', false);
+
+
+        $this->createTable('tbl_auth_item_child', array(
+                'parent' => 'VARCHAR(64) NOT NULL',
+                'child' => 'VARCHAR(64) NOT NULL',
+            ),
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        $this->execute('ALTER TABLE `tbl_auth_item_child` ADD PRIMARY KEY ( `parent` , `child` )');
+        $this->addForeignKey('fk_parent', 'tbl_auth_item_child', 'parent', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_child', 'tbl_auth_item_child', 'child', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE');
+
+
+        $this->createTable('tbl_auth_assignment', array(
+                'item_name' => 'VARCHAR(64) NOT NULL',
+                'user_id' => 'VARCHAR(64) NOT NULL PRIMARY KEY',
+                'biz_rule' => 'text',
+                'data' => 'text'),
+            'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+        $this->addForeignKey('fk_item_name', 'tbl_auth_assignment', 'item_name', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE');
+
+
+
+
         $this->createTable('gal_dir', array(
                 'id' => 'pk',
                 'element_id_cover' => 'integer',
@@ -90,6 +171,12 @@ class m000101_010101_galpho extends \yii\db\Migration
         $this->dropTable('gal_group');
         $this->dropTable('gal_dir');
         $this->dropTable('gal_element');
+        $this->dropTable('tbl_user_field');
+        $this->dropTable('tbl_user_authenticate');
+        $this->dropTable('tbl_user');
+        $this->dropTable('tbl_auth_assignment');
+        $this->dropTable('tbl_auth_item_child');
+        $this->dropTable('tbl_auth_item');
         return true;
     }
 }
