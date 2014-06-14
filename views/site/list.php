39e3aeea35454ca0bc6da63c99f935b10e3a062c
athos99 <athos99@bluewin.ch>
2014-06-15 00:46:04 +0200

    dev

--- views/site/list.php
+++ views/site/list.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * @var app\Galpho $galpho
+ * @var app\galpho\Galpho $galpho
  * @var yii\Web\View $this
  */
 $this->blocks['header1'] = $this->render('//site/subviews/header-list', array('galpho' => &$galpho));
 $this->blocks['block1'] = $this->render('//site/subviews/tree', array('galpho' => $galpho));
 echo $this->render('//site/subviews/list', array('galpho' => $galpho));
 
+use \yii\helpers\Html;
+echo Html::beginForm();
+echo athos99\plupload\PluploadWidget::widget(array());
+echo Html::submitButton();
+echo Html::endForm();
