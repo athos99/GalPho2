10324800cdfea84761293f4731ce0f50a2c950b6
mb@neurones-cie.com <mb@neurones-cie.com>
2015-01-08 18:03:54 +0100

    dev

--- views/site/subviews/list.php
+++ views/site/subviews/list.php
@@ -23,33 +23,47 @@ $list = array_slice($fullList, $pagination->offset, $pagination->limit);
 ?>
 <div class="row"><?php
     foreach ($list as $element) :
-        if ($element['type'] == 'dir') :
-            ?>
-            <div class="galpho-thumb galpho-dir">
-                <a class="thumbnail" href="<?php echo $url . $element['path']; ?>">
-                    <img src="<?php echo $img . '/' . $element['cover']; ?>">
-                </a>
-                <a href="<?php echo $url . $element['path']; ?>">
-                    <div><?php echo $element['title'] ?></div>
-                </a>
+    if ($element['type'] == 'dir') :
+    ?>
+    <div class="galpho-thumb galpho-dir thumbnail">
+        <div class="image">
+            <a href="<?php echo $url . $element['path']; ?>">
+                <img src="<?php echo $img . '/' . $element['cover']; ?>">
+            </a>
 
-                <div><?php echo $element['description'] ?></div>
+            <div class="zone1">
+                <div class="bar">
+                <span class="opacity-background"></span>
+                <span class="forground">
+                    <i class="glyphicon glyphicon-folder-open"></i>
+                    <a href="<?= $url . $element['path']; ?>"><?= $element['title']; ?></a>
+                </span>
+                </div>>
             </div>
-        <?php
-        else :
-            ?>
-            <div class="galpho-thumb galpho-img">
-                <a class="thumbnail" href="<?php echo $url . $element['path']; ?>">
-                    <img src="<?php echo $img . '/' . $element['path']; ?>">
-                </a>
-                <a href="<?php echo $url . $element['path']; ?>">
-                    <div><?php echo $element['title'] ?></div>
-                </a>
+            <div class="zone2">
+                <span class="opacity-background"></span>
+                <span class="forground">zone2</span>
+            </div>
+        </div>
+    </div>
 
-                <div><?php echo $element['description'] ?></div>
-            </div>
-        <?php
-        endif;
-    endforeach;
-    echo yii\Widgets\LinkPager::widget(['pagination' => $pagination,]);
-    ?></div>
+    <div><?php echo $element['description'] ?></div>
+</div>
+<?php
+else :
+?>
+<div class="galpho-thumb galpho-img">
+    <a class="thumbnail" href="<?php echo $url . $element['path']; ?>">
+        <img src="<?php echo $img . '/' . $element['path']; ?>">
+    </a>
+    <a href="<?php echo $url . $element['path']; ?>">
+        <div><?php echo $element['title'] ?></div>
+    </a>
+
+    <div><?php echo $element['description'] ?></div>
+</div>
+<?php
+endif;
+endforeach;
+echo yii\Widgets\LinkPager::widget(['pagination' => $pagination,]);
+?></div>
